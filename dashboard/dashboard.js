d6b93b780316e753dd45159e48969988ca60c053
NguHaozaza123 <88177900+NguHaozaza123@users.noreply.github.com>
2024-03-06 22:27:32 +0700

    add green page to works in real time

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -15,6 +15,143 @@ function updateClock() {
 }
 setInterval(updateClock, 1000);
 updateClock();
-function goBack() {
-    window.history.back();
+
+const setDocument = (circleint, textbox, SYS, DIA, PUL, SYSint, DIAint, PULint, advice, advicestr) => {
+      document.getElementById("circleint").innerHTML = circleint;
+      document.getElementById("textbox").innerHTML = textbox;
+      document.getElementById("SYS").innerHTML = SYS;
+      document.getElementById("DIA").innerHTML =DIA;
+      document.getElementById("PUL").innerHTML =PUL;
+      document.getElementById("SYSint").innerHTML =SYSint;
+      document.getElementById("DIAint").innerHTML =DIAint;
+      document.getElementById("PULint").innerHTML =PULint;
+      document.getElementById("advice").innerHTML =advice;
+      document.getElementById("advicestr").innerHTML =advicestr;
 }
+
+const updateStatus = (statusToUpdate) => {
+  const pageStatusElement = document.getElementById("pageStatus");
+  pageStatusElement.classList.remove(pageStatusElement.className);
+  pageStatusElement.classList.add(statusToUpdate);
+  switch (statusToUpdate) {
+    case "green":
+      setDocument(
+        "ปกติ",
+        "ผลการวัดความดันได้ดังนี้",
+        "ค่าความดันขณะหัวใจบีบตัว: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; mmHg",
+        "ค่าความดันขณะหัวใจคลายตัว:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; mmHg",
+        "อัตราการเตันของหัวใจ:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; /Min",
+        "123",
+        "123",
+        "123",
+        "ยินดีด้วย คุณมีความดันปกติ",
+        "",
+      )
+      break;
+
+    case "red":
+      setDocument(
+        "สูง",
+        "ผลการวัดความดันได้ดังนี้",
+        "ค่าความดันขณะหัวใจบีบตัว: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; mmHg",
+        "ค่าความดันขณะหัวใจคลายตัว:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; mmHg",
+        "อัตราการเตันของหัวใจ:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; /Min",
+        "123",
+        "123",
+        "123",
+        "คำแนะนำ:",
+        "หลีกเลี่ยงอาหารรสเค็ม<br>งดสูบบุหรี่<br>งดดื่มแอลกอฮอ์",
+      )
+      break;
+
+    case "yellow":
+      setDocument(
+        "ต่ำ",
+        "ผลการวัดความดันได้ดังนี้",
+        "ค่าความดันขณะหัวใจบีบตัว: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; mmHg",
+        "ค่าความดันขณะหัวใจคลายตัว:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; mmHg",
+        "อัตราการเตันของหัวใจ:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; /Min",
+        "123",
+        "123",
+        "123",
+        "คำแนะนำ:",
+        "",
+      )
+      break;
+
+    case "error":
+      setDocument(
+        "X",
+        "ERROR",
+        "อ่านค่าผิดพลาดกรุณาลองใหม่อีกครั้ง",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+      )
+      break;
+
+    case "idle":
+      setDocument(
+        "",
+        "PulseMate",
+        "",
+        "",
+        "",
+        "พร้อมให้บริการ",
+        "",
+        "",
+        "",
+        "",
+      )
+
+      break;
+
+    default:
+      break;
+  }
+}
+
+const getData = () => {
+  const Http = new XMLHttpRequest();
+  const url='https://3.1.206.42:1880/getData';
+  Http.open("GET", url);
+  Http.send();
+
+  Http.onreadystatechange = (e) => {
+    const data = JSON.parse(Http.responseText)
+    if (data.error) {
+      updateStatus("error");
+    } else {
+      updateStatus("green");
+      document.getElementById("SYSint").innerHTML = data.sys;
+      document.getElementById("PULint").innerHTML = data.pul;
+      document.getElementById("DIAint").innerHTML = data.dia;
+    }
+  };
+}
+
+const socket = new WebSocket("ws://3.1.206.42:1880/dataUpdate");
+
+
+// Connection opened
+socket.addEventListener("open", (event) => {
+  socket.send("Hello Server!");
+});
+
+// Listen for messages
+socket.addEventListener("message", (event) => {
+  const data = JSON.parse(event.data)
+  if (data.error) {
+    updateStatus("error");
+  } else {
+    updateStatus("green");
+    document.getElementById("SYSint").innerHTML = data.sys;
+    document.getElementById("PULint").innerHTML = data.pul;
+    document.getElementById("DIAint").innerHTML = data.dia;
+  }
+});
+
